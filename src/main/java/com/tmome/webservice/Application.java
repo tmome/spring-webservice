143ef5aa6b5a8c136bf8e2116c5d1a5bd4fb464a
천현승 <mz_hscheon@woowafriends.com>
2022-12-21 14:01:54 +0900

    초기 셋팅 중

--- src/main/java/com/tmome/webservice/Application.java
+++ src/main/java/com/tmome/webservice/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WebserviceApplication {
+public class Application {
 
   public static void main(String[] args) {
-    SpringApplication.run(WebserviceApplication.class, args);
+    SpringApplication.run(Application.class, args);
   }
 
 }
